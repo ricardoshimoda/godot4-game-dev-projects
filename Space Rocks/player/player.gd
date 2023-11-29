bafaab90941041e314e754312932f09f7ad9988f
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2023-11-29 15:52:12 -0500

    exploding stuff

--- Space Rocks/player/player.gd
+++ Space Rocks/player/player.gd
@@ -1,18 +1,40 @@
 extends RigidBody2D
 
+@export var bullet_scene: PackedScene
+@export var fire_rate = 0.25
 @export var engine_power = 500
 @export var spin_power = 8000
+
+signal lives_changed
+signal dead
 
 enum { INIT, ALIVE, INVULNERABLE, DEAD }
 var state = INIT
 var thrust = Vector2.ZERO
 var rotation_dir = 0
 var screensize = Vector2.ZERO
+var can_shoot = true
+var reset_pos = false
+var lives = 0: set = set_lives
 
-# Called when the node enters the scene tree for the first time.
+func set_lives(value):
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
 func _ready():
 	screensize = get_viewport_rect().size
 	change_state(ALIVE)
+	$GunCooldown.wait_time = fire_rate
 
 func change_state(new_state):
 	match new_state:
@@ -34,8 +56,19 @@ func get_input():
 	if state in [ DEAD, INIT ]:
 		return
 	if Input.is_action_pressed("thrust"):
-		thrust = -transform.y * engine_power
+		thrust = transform.x * engine_power
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+
+func shoot():
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
 
 func _physics_process(delta):
 	constant_force = thrust
@@ -46,3 +79,9 @@ func _integrate_forces(state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	state.transform = xform
+	if reset_pos:
+		state.transform.origin = screensize/2
+		reset_pos = false
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
