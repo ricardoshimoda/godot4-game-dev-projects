ca6387082ba984ce599f7ceb1ec9860495311648
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2023-11-29 16:28:47 -0500

    implementing game over

--- Space Rocks/player/player.gd
+++ Space Rocks/player/player.gd
@@ -40,12 +40,19 @@ func change_state(new_state):
 	match new_state:
 		INIT:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
 			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1.0
 		INVULNERABLE:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
 
 func _process(delta):
@@ -85,3 +92,19 @@ func _integrate_forces(state):
 
 func _on_gun_cooldown_timeout():
 	can_shoot = true
+
+func _on_invulnerability_timer_timeout():
+	change_state(ALIVE)
+
+func _on_body_entered(body):
+	if body.is_in_group("rocks"):
+		print("I've hit a rock!")
+		body.explode()
+		lives -= 1
+		explode()
+
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
