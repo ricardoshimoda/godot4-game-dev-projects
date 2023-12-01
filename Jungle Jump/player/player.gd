6a3b2bac6d21f8aec9c00929f24c3883d8cc248c
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2023-12-01 14:49:59 -0500

    creating very basic movement for platform character

--- Jungle Jump/player/player.gd
+++ Jungle Jump/player/player.gd
@@ -1,1 +1,61 @@
 extends CharacterBody2D
+
+@export var gravity = 750
+@export var run_speed = 150
+@export var jump_speed = -300 #interesting
+
+enum { IDLE, RUN, JUMP, HURT, DEAD }
+var state = IDLE
+
+func _ready():
+	change_state(IDLE)
+
+func change_state(new_state):
+	state = new_state
+	match state:
+		IDLE:
+			$AnimationPlayer.play("idle")
+		RUN:
+			$AnimationPlayer.play("run")
+		HURT:
+			$AnimationPlayer.play("hurt")
+		JUMP:
+			$AnimationPlayer.play("jump_up")
+		DEAD:
+			hide()
+		
+func get_input():
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var jump = Input.is_action_just_pressed("jump")
+	
+	velocity.x = 0
+	if right:
+		velocity.x += run_speed
+		$Sprite2D.flip_h = false
+	if left:
+		velocity.x -= run_speed
+		$Sprite2D.flip_h = true
+	if jump and is_on_floor():
+		change_state(JUMP)
+		velocity.y = jump_speed
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+	if state == JUMP and velocity.y > 0:
+		$AnimationPlayer.play("jump_down")
+	if state == IDLE and velocity.x != 0:
+		change_state(RUN)
+	if state == RUN and velocity.x == 0:
+		change_state(IDLE)
+	if state in [IDLE,RUN] and !is_on_floor():
+		change_state(JUMP)
+
+func _physics_process(delta):
+	velocity.y = gravity * delta
+	get_input()
+	move_and_slide()
+
+func reset(_position):
+	position = _position
+	show()
+	change_state(IDLE)
